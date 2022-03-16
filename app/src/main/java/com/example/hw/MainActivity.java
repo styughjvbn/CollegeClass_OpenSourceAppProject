72bedac6df85bd996527ab3f67cf3ddc19404d90
JongWon <3338761@naver.com>
2022-03-16 15:19:54 +0900

    HW2_practice2-1

--- app/src/main/java/com/example/hw/MainActivity.java
+++ app/src/main/java/com/example/hw/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.icon1);
     }
 }
